3085c50caf2038e809820e9e253f019b89533063
rcyou <ryoung@mail.com>
2015-07-25 16:17:49 -0500

    Initial commit. Includes grid.css, global.css, index.html/about.html starter pages, home/about_content includes, jquery, and global.js files

---
+++ js/global.js
@@ -0,0 +1,10 @@
+/* ==========================================================================
+ GLOBAL.JS
+ Global JavaScript functions, events, and actions used throughout the website.
+ ========================================================================== */
+
+
+
+$(document).ready(function() {
+	
+});
